1ea58139aa42bea14ec94a4edaf25bc5f13d0304
loht12 <lorranyoliveiramedeiros937@gmail.com>
2026-06-15 21:22:40 -0300

    programa03_mudancas

---
+++ programa03.py
@@ -0,0 +1,44 @@
+# Calculadora python
+
+num1 = int(input ("Digite o primeiro numero: "))
+
+num2 = int(input ("Digite o segundo numero: "))
+
+num3 = int(input ("Digite o terceiro numero:"))
+
+soma = num1 + num2 + num3
+
+subtracao = num1 - num2 - num3
+
+multiplicacao = num1 * num2 * num3
+
+divisao = num1 / num2 / num3
+
+divisao_inteira = num1 // num2 // num3
+
+resto_divisao = num1 % num2 % num3
+
+potenciacao = num1 ** num2 ** num3
+
+##Operacoes
+
+soma = num1 +num2 + num3
+print(f"A soma de  {num1} e {num2} e {num3} é igual a = {soma}")
+
+subtracao = num1 - num2 - num3
+print(f"A subtracao de {num1} e {num2} e {num3} é igual a = {subtracao}")
+
+multiplicacao = num1 * num2 * num3
+print (f"A multriplicacao de {num1} e {num2} e {num3} é igual a = {multiplicacao}")
+
+divisao = num1 / num2 / num3
+print (f"A divisao de {num1} e {num2} e {num3} é igual a = {divisao}")
+
+divisao_inteira = num1 // num2 // num3
+print (f"A divisao_inteira de {num1} e {num2} e {num3} é igual a = {divisao_inteira}")
+
+resto_divisao = num1 % num2 % num3
+print (f"O resto_divisao de {num1} e {num2} e {num3} é igual a = {resto_divisao}")
+
+potenciacao = num1 ** num2 ** num3
+print (f"A potenciacao de {num1} e {num2} e {num3} é igual a = {potenciacao}")
