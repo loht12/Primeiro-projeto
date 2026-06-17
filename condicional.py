c7811b3406f4aed3233fcced3dcd637005436f74
loht12 <lorranyoliveiramedeiros937@gmail.com>
2026-06-17 20:30:47 -0300

    add_cond_e_par_impar

--- condicional.py
+++ condicional.py
@@ -1,8 +1,10 @@
-n1 = float(input("Digite a nota 1:"))
+n1 = float(input("Digite a primeira nota:"))
 n2 = float(input("Digite a nota 2:"))
 media = (n1 + n2)/2
 
 if media >= 7:
     print ("Aprovado")
+elif media >= 3 and media < 7:
+    print("Recuperação")
 else:
     print ("Reprovado")
