7b09a12c94ad50bb10c7f340741af29278a71175
loht12 <lorranyoliveiramedeiros937@gmail.com>
2026-06-17 18:35:44 -0300

    add_coisas

---
+++ media_aluno.py
@@ -0,0 +1,29 @@
+#Média
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+nota3 = float(input("Digite a terceira nota:"))
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media >= 7:
+    print("Aluno(a) Aprovado(a)")
+else:
+    print("Aluno(a) Reprovado(a)")
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media >= 7:
+        print(f"Aluno(a) Aprovado(a) com media {media:.2f}")
+elif media >= 3 and media < 7:
+    print(f"Aluno(a) em Recuperação com média{media:.2f}")
+    fez_recuperacao = input("Aluno já fez recuperação? s/n:")
+    if fez_recuperacao == "s":
+        nota_recuperacao = float(input("Digite a nota da recuperação: "))
+        if nota_recuperacao >= 5:
+            print("Aluno(a) aprovado pela recuperação")
+        else:
+            print("Aluno(a) não obteve nota suficiente para ser aprovado após a recuperação.")
+
+else:
+    print(f"Aluno(a) Reprovado(a) com media{media:.2f} ")
+
