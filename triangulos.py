3c11999bedb621a28979dbe0ccae9e7836bd9e10
loht12 <lorranyoliveiramedeiros937@gmail.com>
2026-06-18 21:13:29 -0300

    add_att_07

---
+++ triangulos.py
@@ -0,0 +1,9 @@
+comprimento_A = float(input("Digite o comprimento l1 :"))
+comprimento_B = float(input("Digite o comprimento l2:"))
+comprimento_C = float(input("Digite o comprimento l2:"))
+
+if comprimento_A + comprimento_B < comprimento_C or comprimento_A + comprimento_B <comprimento_B or comprimento_B + comprimento_C < comprimento_A :
+    print ("Triangulo inválido")
+else:
+    print ("Triangulo valido")
+    if comprimento_A
